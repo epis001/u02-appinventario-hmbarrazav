e2b97f3a35ed7c420161cc454e5606d98b2ccb16
hmbarrazav <hmbarrazav@gmail.com>
2021-10-21 15:11:21 -0500

    Leyendo productos

--- app/src/main/java/com/example/appinventario/MainActivity.java
+++ app/src/main/java/com/example/appinventario/MainActivity.java
@@ -5,14 +5,29 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class MainActivity extends FragmentActivity {
     private FloatingActionButton fabaddproduct;
     private static final int RC_CREATE_PRODUCT = 1;
     private static final int RC_UPDATE_PRODUCT = 2;
+    Retrofit retrofit; 
+    servicesRetrofit miserviceretrofit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +40,39 @@ public class MainActivity extends FragmentActivity {
                 startActivityForResult(intent, RC_CREATE_PRODUCT);
             }
         });
+        final String url = getString(R.string.midominio);
+        Gson gson = new GsonBuilder().setLenient().create();
+        retrofit = new Retrofit.Builder().baseUrl(url)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        miserviceretrofit = retrofit.create(servicesRetrofit.class);
+        cargarproductos();
     }
+
+    private void cargarproductos() {
+        Call<List<Productos>> call = miserviceretrofit.getlist();
+        call.enqueue(new Callback<List<Productos>>() {
+            @Override
+            public void onResponse(Call<List<Productos>> call, Response<List<Productos>> response) {
+                Log.e("mirespuesta: ", response.toString());
+                for(Productos res : response.body()) {
+                    Log.e("mirespuesta: ","id="+res.getId()+" prod="+res.getName() +" precio"+res.getPrice());
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Productos>> call, Throwable t) {Log.e("onFailure", t.toString()); }
+        });
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_CREATE_PRODUCT) {
-            //cargarproductos();
+            cargarproductos();
         } else if (requestCode == RC_UPDATE_PRODUCT) {
-            // cargarproductos();
+            cargarproductos();
         }
     }
 }
--- app/src/main/java/com/example/appinventario/servicesRetrofit.java
+++ app/src/main/java/com/example/appinventario/servicesRetrofit.java
@@ -1,10 +1,15 @@
 package com.example.appinventario;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface servicesRetrofit {
     @POST("products/create")
     Call<String> newproducto(@Body Productos producto);
+    @GET("products/list")
+    Call<List<Productos>> getlist();
 }
